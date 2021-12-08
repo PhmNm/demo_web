adb0843e1d5308f195c40833805fe8b6f481e499
PhmNm <19120597@student.hcmus.edu.vn>
2021-12-09 01:13:04 +0700

    change 5 -> 10

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './components/HomePage/HomePage';
 function App() {
   return (
     <div className="App">
-      <HomePage itemsPerPage={5} />
+      <HomePage itemsPerPage={10} />
     </div>
 
   );
